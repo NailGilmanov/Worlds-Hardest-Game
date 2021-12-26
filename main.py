aed7f96c58049941a27e06be9c068bfc3bb18318
NailGilmanov <NailGIlmanov27@mail.ru>
2021-12-26 22:11:27 +0300

    load_image function.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 import pygame
 
 pygame.init()
@@ -5,6 +8,26 @@ size = width, height = 300, 300
 screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption('Worlds hardest game!!!!!!!')
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+
+    image = pygame.image.load(fullname)
+
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+    return image
 
 
 running = True
